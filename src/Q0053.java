3221d8d0d5ab09c2f68fe09b8d40614c1dccf654
CZ <zzhhdsf@gmail.com>
2021-07-10 18:36:42 -0400

    Q0053

--- src/Q0053.java
+++ src/Q0053.java
@@ -1,10 +1,12 @@
 public class Q0053 {
     public static int maxSubArray(int[] nums) {
-        int result = Integer.MIN_VALUE;
+        int result = -100000;
         int temp = result;
         for(int i = 0;i<nums.length;i++){
-            =
+            temp = temp+nums[i]>=nums[i]? temp+nums[i]:nums[i];
+            result = temp>=result? temp : result;
         }
+        return result;
     }
 
     public static void main(String[] args) {
