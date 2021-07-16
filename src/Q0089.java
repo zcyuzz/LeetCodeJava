b7d77dcb66fb2a14eed25a6a669b949bcff6872e
CZ <zzhhdsf@gmail.com>
2021-07-15 21:10:31 -0400

    Q0611

---
+++ src/Q0089.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q0089 {
+    public static List<Integer> grayCode(int n) {
+        ArrayList<String> temp = new ArrayList<>();
+        temp.add("0");
+        temp.add("1");
+        ArrayList<String> temp2 = new ArrayList<>();
+        for(int i=1;i<n;i++){
+            for(int j=0;j<temp.size();j++){
+                temp2.add("0"+temp.get(j));
+            }
+            for(int j=temp.size()-1;j>=0;j--){
+                temp2.add("1"+temp.get(j));
+            }
+            temp.clear();
+            temp.addAll(temp2);
+            temp2.clear();
+        }
+        ArrayList<Integer> result = new ArrayList<>();
+        for(int i=0;i<temp.size();i++){
+            result.add(Integer.parseInt(temp.get(i),2));
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+       grayCode(15);
+        System.out.println(grayCode(28));
+    }
+
+}
+
