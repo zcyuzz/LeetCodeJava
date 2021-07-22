ccd12728bb9c815e71db7d8d83389b4e72594efe
CZ <zzhhdsf@gmail.com>
2021-07-21 21:29:53 -0400

    Q1773

---
+++ src/Q0942.java
@@ -0,0 +1,13 @@
+public class Q0942 {
+    public int[] diStringMatch(String s) {
+        int end = s.length();
+        int tempEnd = end;
+        int start = 0;
+        int [] result = new int[end+1];
+        for(int i =0;i<end;i++){
+            result[i]= s.charAt(i)=='I'? start++:tempEnd--;
+        }
+        result[end]=start;
+        return result;
+    }
+}
