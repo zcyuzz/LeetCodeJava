faec5f43c6d36fe7a3d7886404513270265873e9
CZ <zzhhdsf@gmail.com>
2021-07-26 23:56:47 -0400

    Q0239 with brute force and Time Limit Exceeded

--- src/Q0832.java
+++ src/Q0832.java
@@ -8,12 +8,7 @@ public class Q0832 {
             temp = image[i].clone();
             for (int j = 0; j < image[i].length; j++) {
                 image[i][j] = temp[n - j - 1];
-            }
-        }
-        for (int i = 0; i < image.length; i++) {
-            for (int j = 0; j < image[i].length; j++) {
-                if (image[i][j] == 0) image[i][j] = 1;
-                else image[i][j] = 0;
+                image[i][j]=image[i][j]==1?0:1;
             }
         }
         return image;
---
+++ src/Q1290.java
@@ -0,0 +1,25 @@
+public class Q1290 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {
+        }
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public int getDecimalValue(ListNode head) {
+        int result = 0;
+        while (head != null) {
+            result = result*2 + head.val;
+            head = head.next;
+        }
+        return result;
+    }
+}
+
