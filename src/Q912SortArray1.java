cef29d2aebe9ff8a86a0bd9412ea31b7e1294e3c
CZ <zzhhdsf@gmail.com>
2021-07-29 18:31:41 -0400

    Q912 with selection and bubble sort

--- src/Q912SortArray1.java
+++ src/Q912SortArray1.java
@@ -1,5 +1,40 @@
+
+
 public class Q912SortArray1 {
-    public int[] sortArray(int[] nums) {
+    public static void main(String[] args) {
+        //generate random int array
+        int [] random = new int[100000];
+        for(int i =0;i<random.length;i++){
+            random[i]= (int) (Math.random()*100000);
+        }
+        //Selection Sort
+        long start = System.nanoTime();
+        selectionSortArray(random.clone());
+        long end = System.nanoTime();
+        System.out.println("Selection sort: " + (end - start) + "ns");
+        //Bubble Sort
+        start = System.nanoTime();
+        bubbleSortArray(random.clone());
+        end = System.nanoTime();
+        System.out.println("Bubble sort: " + (end - start) + "ns");
+    }
+
+    //bubble sort
+    public static int[] bubbleSortArray(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - i-1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+
+    //selection sort
+    public static int[] selectionSortArray(int[] nums) {
         int minIndex = 0;
         int temp = 0;
         for (int i = 0; i < nums.length - 1; i++) {
@@ -13,4 +48,6 @@ public class Q912SortArray1 {
         }
         return nums;
     }
+
+
 }
