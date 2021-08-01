b0ac83c127b0e0c8ce12fcff3bc6845add7e0528
CZ <zzhhdsf@gmail.com>
2021-07-31 23:57:10 -0400

    Q1710

--- src/Q1710.java
+++ src/Q1710.java
@@ -1,13 +1,12 @@
-
-
 public class Q1710 {
-    public static int maximumUnits(int[][] boxTypes, int truckSize) {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
         int result =0;
         int[] boxSize = new int[1001];
         for(int i =0;i<boxTypes.length;i++){
             boxSize[boxTypes[i][1]]+=boxTypes[i][0];
         }
         for(int i =boxSize.length-1;i>=0;i--){
+            if(truckSize==0)continue;
             result+=boxSize[i]*i;
             truckSize-=boxSize[i];
             if(truckSize<=0){
@@ -16,10 +15,5 @@ public class Q1710 {
             }
         }
         return result;
-    }
-
-    public static void main(String[] args) {
-        int[][] temp = new int[][]{{1,3},{2,2},{3,1}};
-        System.out.println(maximumUnits(temp,4));
     }
 }
