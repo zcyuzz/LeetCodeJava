374f13c3536aa0188ee5ca329d0ce219d99a633e
CZ <zzhhdsf@gmail.com>
2021-08-02 05:25:27 -0400

    Leetcode

--- src/Q0001.java
+++ src/Q0001.java
@@ -1,16 +1,34 @@
+import java.util.HashMap;
+
 public class Q0001 {
     public int[] twoSum(int[] nums, int target) {
-        int[] answer =new int[2];
+        //hashmap
+        int[] result =new int[2];
+        HashMap<Integer,Integer> numbers = new HashMap<>();
         for(int i =0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    answer[0] =i;
-                    answer[1] =j;
-                }
+            numbers.put(nums[i],i);
+        }
+        for(int i =0;i<nums.length;i++){
+            int temp = target - nums[i];
+            if(numbers.containsKey(temp)&&numbers.get(temp)!=i){
+                result[0]=i;
+                result[1]=numbers.get(temp);
+                return result;
             }
         }
-
-        return answer;
+        return result;
+        //brute force
+//        int[] answer =new int[2];
+//        for(int i =0;i<nums.length;i++){
+//            for(int j=i+1;j<nums.length;j++){
+//                if(nums[i]+nums[j]==target){
+//                    answer[0] =i;
+//                    answer[1] =j;
+//                }
+//            }
+//        }
+//
+//        return answer;
     }
 
 }
--- src/Q0003.java
+++
@@ -1,9 +0,0 @@
-import java.util.HashSet;
-
-public class Q0003 {
-    public int lengthOfLongestSubstring(String s) {
-        int result =1;
-        HashSet<Character> characters= new HashSet<>();
-        for(int i =0;i<s.length();i++)
-    }
-}
