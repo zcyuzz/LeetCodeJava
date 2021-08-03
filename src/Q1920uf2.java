aa177e583c3bdcfdc8853e201dc441387d59d5e8
Cao Zheng <Cao.Zheng@stonybrook.edu>
2021-08-02 21:51:05 -0400

    Leetcode

--- src/Q1920uf2.java
+++ src/Q1920uf2.java
@@ -1,16 +1,14 @@
 public class Q1920uf2 {
     public static int[] buildArray(int[] nums) {
+
+        return new int [2];
         //need extra space
 //        int [] result = new int[nums.length];
 //        for(int i = 0;i< nums.length;i++){
 //            result[i]=nums[nums[i]];
 //        }
 //        return result;
-        // space O(1)
-        for (int i = 0; i < nums.length; i++) {
 
-        }
-        return new int [2];
     }
 
     public static void main(String[] args) {
