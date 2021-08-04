eadec1ab5a2066671b1a87fa9b0259384e320133
CZ <zzhhdsf@gmail.com>
2021-08-04 07:09:05 -0400

    Insertion sort

--- src/Q912SortArray1.java
+++ src/Q912SortArray1.java
@@ -1,20 +1,41 @@
+
+
 public class Q912SortArray1 {
     public static void main(String[] args) {
         //generate random int array
-        int[] random = new int[100000];
+        int[] random = new int[50000];
         for (int i = 0; i < random.length; i++) {
-            random[i] = (int) (Math.random() * 100000);
+            random[i] = (int) (Math.random() * 50000);
         }
-        //Selection Sort
-        long start = System.nanoTime();
+//        Selection Sort
+        long start = System.currentTimeMillis();
         selectionSortArray(random.clone());
-        long end = System.nanoTime();
-        System.out.println("Selection sort: " + (end - start) + "ns");
-        //Bubble Sort
-        start = System.nanoTime();
+        long end = System.currentTimeMillis();
+        System.out.println("Selection sort: " + (end - start) + "ms");
+//        Bubble Sort
+        start = System.currentTimeMillis();
         bubbleSortArray(random.clone());
-        end = System.nanoTime();
-        System.out.println("Bubble sort: " + (end - start) + "ns");
+        end = System.currentTimeMillis();
+        System.out.println("Bubble sort: " + (end - start) + "ms");
+//        Insertion Sort
+        start = System.currentTimeMillis();
+        insertionSortArray(random.clone());
+        end = System.currentTimeMillis();
+        System.out.println("Insertion sort: " + (end - start) + "ms");
+    }
+
+    //insertion sort
+    public static int[] insertionSortArray(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            int key = nums[i];
+            int j = i - 1;
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            nums[j + 1] = key;
+        }
+        return nums;
     }
 
     //bubble sort
