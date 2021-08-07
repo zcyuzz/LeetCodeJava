590547308ab8a41df7853f00502c0b9dfe920ad8
CZ <zzhhdsf@gmail.com>
2021-08-07 00:11:34 -0400

    leetcode

--- src/Q0977.java
+++ src/Q0977.java
@@ -1,13 +1,31 @@
-import java.util.Arrays;
+
 
 public class Q0977 {
     public int[] sortedSquares(int[] nums) {
-        //brute force
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = nums[i] * nums[i];
+        //two pointer
+        if (nums.length == 1) return new int[]{nums[0] * nums[0]};
+        int[] temp = new int[nums.length];
+        int i = 0, j = nums.length - 1;
+        int index = 0;
+        while (i != j) {
+            if (Math.abs(nums[i]) >= Math.abs(nums[j])) {
+                temp[index++] = nums[i] * nums[i];
+                i++;
+            } else {
+                temp[index++] = nums[j] * nums[j];
+                j--;
+            }
         }
-        ;
-        Arrays.sort(nums);
+        temp[nums.length - 1] = nums[i] * nums[i];
+        for (int k = 0; k < nums.length; k++) {
+            nums[k] = temp[temp.length - k - 1];
+        }
         return nums;
+        //brute force
+//        for(int i =0;i<nums.length;i++){
+//            nums[i]=nums[i]*nums[i];
+//        };
+//        Arrays.sort(nums);
+//        return nums;
     }
 }
