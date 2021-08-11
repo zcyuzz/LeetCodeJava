2d3311842e4069fbba690167049f3a5ba8c8b7aa
CZ <zzhhdsf@gmail.com>
2021-08-10 23:02:52 -0400

    leetcode

--- src/Q0026.java
+++ src/Q0026.java
@@ -1,17 +1,18 @@
 public class Q0026 {
     public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
         int result = nums.length;
         int[] temp = new int[nums.length];
         temp[0] = nums[0];
         int index = 1;
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) continue;
+            if (nums[i] == nums[i - 1]) {
+                result--;
+                continue;
+            }
             temp[index++] = nums[i];
-            result--;
         }
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = temp[i];
-        }
+        for (int i = 0; i < nums.length; i++) nums[i] = temp[i];
         return result;
     }
 }
