459d34f6576d333e2c6391f8023121ab5cd9be91
CZ <zzhhdsf@gmail.com>
2021-08-11 12:24:58 -0400

    leetcode

--- src/Q0026.java
+++ src/Q0026.java
@@ -1,18 +1,28 @@
 public class Q0026 {
     public int removeDuplicates(int[] nums) {
-        if (nums.length == 0) return 0;
+        // two pointer
         int result = nums.length;
-        int[] temp = new int[nums.length];
-        temp[0] = nums[0];
+        int current = nums[0];
         int index = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {
+        for (int i = 1; i < result; i++) {
+            if (nums[i] == current) {
                 result--;
                 continue;
             }
-            temp[index++] = nums[i];
+            current = nums[i];
+            nums[index++] = nums[i];
         }
-        for (int i = 0; i < nums.length; i++) nums[i] = temp[i];
         return result;
+        //extra array
+//        if (nums.length == 0) return 0;
+//        int result = nums.length, index = 1;
+//        int[] temp = new int[nums.length];
+//        temp[0] = nums[0];
+//        for (int i = 1; i < nums.length; i++) {
+//            if (nums[i] == nums[i - 1]) result--;
+//            else temp[index++] = nums[i];
+//        }
+//        for (int i = 0; i < nums.length; i++) nums[i] = temp[i];
+//        return result;
     }
 }
