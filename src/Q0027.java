2f0f50ee4e788ae7944bcf4f9752c512ecdb3558
CZ <zzhhdsf@gmail.com>
2021-08-11 12:35:37 -0400

    leetcode

--- src/Q0027.java
+++ src/Q0027.java
@@ -1,15 +1,27 @@
 public class Q0027 {
     public int removeElement(int[] nums, int val) {
+        //without extra space
         int result = nums.length;
-        int[] temp = new int[result];
         int index = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) temp[index++] = nums[i];
-            else result--;
-        }
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = temp[i];
+            if (nums[i] == val) {
+                result--;
+                continue;
+            }
+            nums[index++] = nums[i];
         }
         return result;
+        //extra array
+//        int result = nums.length;
+//        int[] temp = new int[result];
+//        int index = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//            if (nums[i] != val) temp[index++] = nums[i];
+//            else result--;
+//        }
+//        for (int i = 0; i < nums.length; i++) {
+//            nums[i] = temp[i];
+//        }
+//        return result;
     }
 }
