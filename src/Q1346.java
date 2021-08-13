84fed0523fad8478c624f96d923d4c8a0e2c9f29
CZ <zzhhdsf@gmail.com>
2021-08-13 00:06:15 -0400

    leetcode

---
+++ src/Q1346.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+
+public class Q1346 {
+    public boolean checkIfExist(int[] arr) {
+        HashSet<Integer> nums = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (nums.contains(arr[i] * 2) || (nums.contains(arr[i] / 2) && arr[i] / 2 == 0)) return true;
+            nums.add(arr[i]);
+        }
+        return false;
+    }
+}
