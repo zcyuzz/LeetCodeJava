7640d829b414bca89ae443177a1dde900a389412
CZ <zzhhdsf@gmail.com>
2021-08-14 09:01:57 -0400

    leetcode

---
+++ src/Q1351.java
@@ -0,0 +1,16 @@
+public class Q1351 {
+    public int countNegatives(int[][] grid) {
+        //Time: O(m+n)
+        int result = 0;
+
+
+        //brute force Time: O(m * n)
+//        int result = 0;
+//        for (int i = 0; i < grid.length; i++) {
+//            for (int j = 0; j < grid[i].length; j++) {
+//                if (grid[i][j] < 0) result++;
+//            }
+//        }
+//        return result;
+    }
+}
