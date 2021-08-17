81a10301bca7206f0f3f42c9084d2d778381611c
CZ <zzhhdsf@gmail.com>
2021-08-16 23:33:43 -0400

    leetcode

--- src/Q0003uf.java
+++ src/Q0003uf.java
@@ -1,7 +1,18 @@
+import java.util.HashMap;
+
 public class Q0003uf {
     public int lengthOfLongestSubstring(String s) {
-
-
+        int result = 0, index = -1;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (map.containsKey(ch)) {
+                if (map.get(ch) > index) index = map.get(ch);
+            }
+            if (i - index > result) result = i - index;
+            map.put(ch, i);
+        }
+        return result;
     }
 }
 
