c063bc71fb96c15970aae64f26f075dcf1880bc5
Cao Zheng <Cao.Zheng@stonybrook.edu>
2021-08-25 12:48:27 -0400

    Leetcode

--- src/test.java
+++ src/test.java
@@ -4,25 +4,9 @@ public class test {
         s = s.replace('[', '{');
         s = s.replace(']', '}');
         System.out.println(s);
-        String s1 = "";
-        for (int i = 0; i < 100000; i++) {
-            s1 += (char) ((int) (Math.random() * 100));
+        for (int i = 0; i <= 500; i++) {
+            System.out.println(i);
         }
-        System.out.println(s1);
-        int count1 = 0;
-        int count2 = 0;
-        long start = System.nanoTime();
-        for (int i = 0; i < s1.length(); i++) {
-            if (s1.charAt(i) == 1) count1++;
-        }
-        long end = System.nanoTime();
-        System.out.println(start - end);
-        start = System.nanoTime();
-        for (char c : s1.toCharArray()) {
-            if (c == 1) count2++;
-        }
-        end = System.nanoTime();
-        System.out.println(start - end);
 
 
     }
