3a4002cd8051bc93ff254d8ee3387b9d645e95e9
Cao Zheng <Cao.Zheng@stonybrook.edu>
2021-08-25 17:27:21 -0400

    Leetcode

---
+++ src/Q0557.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Q0557 {
+    public String reverseWords(String s) {
+        ArrayList<String> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') sb.append(s.charAt(i));
+            else {
+                list.add(sb.toString());
+                sb.setLength(0);
+            }
+        }
+        list.add(sb.toString());
+        sb.setLength(0);
+        for (int i = list.size() - 1; i >= 0; i--) {
+            sb.append(list.get(i) + ' ');
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
+    }
+}
