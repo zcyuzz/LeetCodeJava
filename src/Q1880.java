69eb4ebbf46ff4d10709782b042f1bdf2bc2c970
Cao Zheng <Cao.Zheng@stonybrook.edu>
2021-08-26 13:31:00 -0400

    Leetcode

--- src/Q1880.java
+++ src/Q1880.java
@@ -1,3 +1,14 @@
 public class Q1880 {
-   b
+    public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
+        return stringToInt(firstWord) + stringToInt(secondWord) == stringToInt(targetWord);
+    }
+
+    public int stringToInt(String s) {
+        int result = 0;
+        for (char ch : s.toCharArray()) {
+            result *= 10;
+            result += ch - 'a';
+        }
+        return result;
+    }
 }
