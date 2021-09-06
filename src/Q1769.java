f33286a05f2eb13152b8b585242f74397fa2a0ec
CZ <zzhhdsf@gmail.com>
2021-09-05 20:33:06 -0400

    Leetcode

---
+++ src/Q1769.java
@@ -0,0 +1,13 @@
+public class Q1769 {
+    public int[] minOperations(String boxes) {
+        int[] result = new int[boxes.length()];
+        for (int i = 0; i < boxes.length(); i++) {
+            if (boxes.charAt(i) == '1') {
+                for (int j = 0; j < boxes.length(); j++) {
+                    result[j] += Math.abs(j - i);
+                }
+            }
+        }
+        return result;
+    }
+}
