8814e01773ca31c59f44d0918bdb271fa0bb8847
CZ <zzhhdsf@gmail.com>
2021-09-08 18:45:59 -0400

    Leetcode

---
+++ src/Q1603.java
@@ -0,0 +1,14 @@
+public class Q1603 {
+}
+
+class ParkingSystem {
+    private int[] slots;
+
+    public ParkingSystem(int big, int medium, int small) {
+        slots = new int[]{big, medium, small};
+    }
+
+    public boolean addCar(int carType) {
+        return slots[carType - 1]-- > 0;
+    }
+}
